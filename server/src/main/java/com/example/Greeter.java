e279add86b66d122f04279dcfeb92973772ec715
backbeat99 <picoesquina@gmail.com>
2018-08-06 15:26:40 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   * @param el nombre de alguien
-   * @return el mensaje a devolver
+   * @param someone el nombre de alguien
+   * @return greeting el mensaje a devolver
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
